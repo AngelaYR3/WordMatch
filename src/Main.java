59e072893da95790a1fd7f728a8db7166417341a
Angela <angelayourant@nycstudents.net>
2025-10-23 23:01:53 -0400

    Fixed WordMatch h

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         System.out.println(w.scoreGuess("issipp"));
         System.out.println(w.scoreGuess("mississippi"));
 
-        WordMatch baby  = new WordMatch("aaaabb");
-        System.out.println(w.scoreGuess("a"));
-        System.out.println(w.scoreGuess("aa"));
-        System.out.println(w.scoreGuess("aaa"));
-        System.out.println(w.scoreGuess("aabb"));
-        System.out.println(w.scoreGuess("c"));
+        WordMatch h = new WordMatch("aaaabb");
+        System.out.println(h.scoreGuess("a"));
+        System.out.println(h.scoreGuess("aa"));
+        System.out.println(h.scoreGuess("aaa"));
+        System.out.println(h.scoreGuess("aabb"));
+        System.out.println(h.scoreGuess("c"));
 
         WordMatch game = new WordMatch("concatenation");
         System.out.println(game.scoreGuess("ten"));
